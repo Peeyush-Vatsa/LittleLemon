705c0c4137a2743e52f7715821bf3d37c481fb0b
Peeyush Vatsa <peeyushvatsa@Peeyushs-MacBook-Air-6.local>
2023-10-09 23:24:59 +0530

    Implementing API auth for users using djoser

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -3,13 +3,16 @@ from django.db import models
 # Create your models here.
 
 class Booking(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=11)
+    id = models.IntegerField(primary_key=True, auto_created=True)
     Name = models.CharField(max_length=255)
-    No_of_guests = models.IntegerField(max_length=6)
+    No_of_guests = models.IntegerField()
     BookingDate = models.DateTimeField()
 
 class Menu(models.Model):
-    id = models.IntegerField(primary_key=True, max_length=5)
+    id = models.IntegerField(primary_key=True, auto_created=True)
     Title = models.CharField(max_length=255)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField(max_length=5)
+    Inventory = models.IntegerField()
+
+    def get_item(self):
+        return f'{self.Title} : {str(self.Price)}'
